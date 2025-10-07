535c8a40e101762cda806f3c4f8dcca65a2a39da
Ivan Obeso <ivan.obeso1@gmail.com>
2025-10-07 20:13:03 +0100

    fixed small naming bug

--- src/commands.ts
+++ src/commands.ts
@@ -14,6 +14,7 @@ export const initTerraformProject = async (template: string) => {
   // Logic to initialize a Terraform project based on the selected template
   console.log(`Initializing Terraform project with template: ${template}`);
 
+  await execa("rm", ["-rf", "infra/.terraform"]);
   await execa("terraform", ["init"], { cwd: "infra", stdio: "inherit" });
   await execa("terraform", ["apply", "-auto-approve"], { cwd: "infra", stdio: "inherit" });
 };
